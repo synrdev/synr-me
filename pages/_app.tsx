9d535a769ff23e0b527a03ecb05587861b234cd3
synrdev <138139785+synrdev@users.noreply.github.com>
2024-10-06 12:14:18 +0900

    Add global CSS styles to the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
+import "../styles/app.css";
 import "@fontsource/jetbrains-mono";
 
 export default function App({ Component, pageProps }: AppProps) {
